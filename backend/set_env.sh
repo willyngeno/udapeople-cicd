93239e8bf1b19278c554e83818a109e910f83d47
willy ngeno <willyngeno60@gmail.com>
2022-07-27 12:49:08 +0300

    This file been modified

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -1,9 +1,9 @@
 export  NODE_ENV=production
 export  VERSION=1
 export  TYPEORM_CONNECTION=postgres
-export  TYPEORM_MIGRATIONS_DIR=./src/migrations
-export  TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export  TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export  TYPEORM_MIGRATIONS_DIR=./migrations
+export  TYPEORM_ENTITIES=./modules/domain/**/*.entity.ts
+export  TYPEORM_MIGRATIONS=./migrations/*.ts
 export  TYPEORM_HOST=localhost
 export  TYPEORM_PORT=5532
 export  TYPEORM_USERNAME=postgres
